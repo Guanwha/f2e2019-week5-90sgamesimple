7e2aae77a17cdc957c71d7c3c96dafa018916b28
Eric Fei <guanwha@gmail.com>
2019-08-16 00:48:21 +0800

    Implemented gameStart page
    1. implement /js/gameStart.js
       - add gamecover.png
       - add button_play.png
       - add button_hint.png
       - add hint.png
       - add button_close.png

--- js/gameStart.js
+++ js/gameStart.js
@@ -2,8 +2,35 @@
 const gameStart = {
     key: 'gameStart',
     preload: function(){
+        this.load.image('bg1', '../assets/gamecover.png');
+        this.load.image('btnPlay', '../assets/button_play.png');
+        this.load.image('btnHint', '../assets/button_hint.png');
+        this.load.image('bgHint', '../assets/hint.png');
+        this.load.image('btnClose', '../assets/button_close.png');
     },
     create: function(){
+        // game start screen
+        this.add.image(0, 0, 'bg1').setOrigin(0);
+        this.btnPlay = this.add.image(803, 577, 'btnPlay').setOrigin(0).setInteractive();
+        this.btnHint = this.add.image(908, 599, 'btnHint').setOrigin(0).setInteractive();
+
+        // hint dialog
+        this.bgHint = this.add.image(0, 0, 'bgHint').setOrigin(0);
+        this.btnClose = this.add.image(816.5, 30.5, 'btnClose').setOrigin(0).setInteractive();
+        this.dialogHint = this.add.container(437, 54, [this.bgHint, this.btnClose]);
+        this.dialogHint.visible = false;
+
+        // interaction
+        this.btnPlay.on('pointerup', () => {
+          console.log('press the play button');
+          this.scene.start('gamePlay');
+        });
+        this.btnHint.on('pointerup', () => {
+          this.dialogHint.visible = true;
+        });
+        this.btnClose.on('pointerup', () => {
+          this.dialogHint.visible = false;
+        });
     },
     update: function(){
     }
